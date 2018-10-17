48b699c3565d78c9666113f573f564ac59a7b9ce
abuduru <abdulg1995@gmail.com>
2018-10-17 15:39:13 -0400

    Burger: Added number of each ingredient to state Dynamically outputs transformed ingredients from Burger builder state using by converting them from an object in state to an array holding the numbers of each ingredient BurgerBuilder: Stores ingredients in state  the passes ingredients as props to burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import  classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return[...Array(props.ingredients[igKey])].map((_,i) => {
+                return <BurgerIngredient key={igKey +i} type={igKey} />
+            });
+        });
     return(
        <div className ={classes.Burger}>
            <BurgerIngredient type="top-bun" />
-           <BurgerIngredient type="cheese" />
-           <BurgerIngredient type="meat" />
+            {transformedIngredients}
            <BurgerIngredient type="bottom-bun" />
        </div> 
     );
