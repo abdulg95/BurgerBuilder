5f9265e7caa9def60c627ff0ce6b634551e01fdf
abuduru <abdulg1995@gmail.com>
2018-10-22 00:27:20 -0400

    Button: created custom buttons with styling for succes and danger OrderSummary: Added total price BurgerBuilder:passed handlers as props into ordersummary to be used as onclick functions for custom buttons

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Aux from '../../../hoc/Auxiliary'
-
+import Aux from '../../../hoc/Auxiliary';
+import Button from '../../UI/Button/Button';
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey =>{
@@ -16,7 +16,10 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Totalprice: {props.price} </strong></p>
             <p>Continue to checkout?</p>
+            <Button btnType="Danger" clicked = {props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked = {props.purchaseContinued}>CONTINUE</Button>
 
         </Aux>
     )
