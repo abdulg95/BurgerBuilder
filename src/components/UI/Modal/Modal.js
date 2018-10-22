240d1c5ff074ef5fecfc05adc1e7d5d70c886058
abuduru <abdulg1995@gmail.com>
2018-10-21 22:17:42 -0400

    Enable cancelling backdrop Backdrop: -Added purchase cancel handle to be used for cancelling backdrop and modal -Passed purchase cancel handler as prop to Modal. -added css for backdrop Modal: -imported higher order auxiliary class and backdrop -Added backdrop with click handler Backdrop: -based on condition show transparent black backdrop

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,8 +1,11 @@
 import React from'react';
-import classes from './Modal.module.css'
-
+import classes from './Modal.module.css';
+import Aux from '../../../hoc/Auxiliary';
+import Backdrop from '../Backdrop/Backdrop';
 const modal = (props) => (
-    <div 
+    <Aux>
+    <Backdrop show={props.show} clicked={props.modalClosed}/>
+     <div 
      className = {classes.Modal}
      style={{
        transform: props.show ? 'translateY(0)': 'translateY(-100vh)',
@@ -10,6 +13,9 @@ const modal = (props) => (
      }}>
         {props.children}
     </div>
+
+    </Aux>
+    
 )
 
 export default modal;
