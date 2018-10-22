7faee4d762cc627fc6dbe0858e4177b35e74a4ae
abuduru <abdulg1995@gmail.com>
2018-10-21 20:06:50 -0400

    Animated modal by hiding and showing BurgerBuilder:  Added property to show if order has been clicked in state passed this to modal. created handler to update this property then passed this as a prop to buildcontrols BuildControls:set purchasing to true on order button click Modal: added anmations to hide and show

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchaseable: false
+        purchaseable: false,
+        purchasing: false
     }
 
     updatePurchaseState (ingredients) {
@@ -73,6 +74,10 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
 
     }
+
+    purchaseHandler = () =>{
+        this.setState({purchasing: true})
+    }
     
     render(){
         const disabledInfo = {
@@ -83,7 +88,7 @@ class BurgerBuilder extends Component {
         }
         return(
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients = {this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
@@ -91,6 +96,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded= {this.addIngredientHandler}
                     ingredientRemoved= {this.removeIngredientHandler}
                     disabled = {disabledInfo}
+                    ordered = {this.purchaseHandler}
                     purchaseable = {this.state.purchaseable}
                     price = {this.state.totalPrice}  />
             </Aux>
