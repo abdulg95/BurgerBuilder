195b810cc30de773777d6de51083e91a6f2c8c67
abuduru <abdulg1995@gmail.com>
2018-10-24 12:16:57 -0400

    Converting to classes in order to use life cycle methods Modal: added should component update check so modal is not rerendered unless it is shown. This also covers order summary as it is wrapped within the modal OrderSummary:now only updates when modal is shown

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component{
+    //this does not need to be a class should be afunctional component
+    componentWillUpdate(){
+        console.log('[OrderSummary] WillUpdate');
+    }
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey =>{
             return <li key = {igKey}>
-                        <span style = {{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                        <span style = {{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
                     </li>
         });
-    
-    return(
-        <Aux>
-            <h3>Your Order</h3>
-            <p>Here's whats in you burger:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Totalprice: {props.price} </strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked = {props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked = {props.purchaseContinued}>CONTINUE</Button>
 
-        </Aux>
-    )
+        return(
+            <Aux>
+                <h3>Your Order</h3>
+                <p>Here's whats in you burger:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Totalprice: {this.props.price} </strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked = {this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked = {this.props.purchaseContinued}>CONTINUE</Button>
+    
+            </Aux>
+        );
+    }
 }
-export default orderSummary;
+export default OrderSummary;
