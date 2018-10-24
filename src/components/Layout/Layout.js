1ad22e5448680406eea74d0478006d15fb73b9ec
abuduru <abdulg1995@gmail.com>
2018-10-24 02:04:52 -0400

    Layout: -Added sidedrawertogglehandler then passed it as prop to toolbar -Toolbar:replaced menu place holder with sidedrawer toggler -DrawerToggle: Created component for toggling sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,9 +3,15 @@ import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 class Layout extends Component{
     state = {
-        showSideDrawer:true
+        showSideDrawer: false
+    }
+    sideDrawerToggleHandler = () =>{
+        this.setState((prevState) =>{
+            return {showSideDrawer: !prevState.showSideDrawer};
+        } );
     }
 
     sideDrawerClosedHandler = () =>{
@@ -14,7 +20,7 @@ class Layout extends Component{
     render(){
         return(
             <Aux>
-                <Toolbar/>
+                <Toolbar drawerToggleClicked = {this.sideDrawerToggleHandler}/>
                 <SideDrawer
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler}/>
