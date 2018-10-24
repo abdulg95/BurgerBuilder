5c498c4e2277fb5cf34612734d1d887c1bde4fff
abuduru <abdulg1995@gmail.com>
2018-10-24 01:08:54 -0400

    Layout: changed from a stateless component  to a stateful one to store states of backdrop Sidedrawer:now has backdrop although can not be toggled yet

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 const sideDrawer = (props) => {
+    let attachedClasses =[classes.SideDrawer,classes.Close];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer,classes.Open];
 
+    }
     return (
-        <div className={classes.SideDrawer}>
-            <Logo height="11%"/>
-            <nav>
-                <NavItems />
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <Logo height="11%"/>
+                <nav>
+                    <NavItems />
+                </nav>
+            </div>
+
+        </Aux>
+       
     )
 }
 
