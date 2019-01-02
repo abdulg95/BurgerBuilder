e8197d660561a5aed22df761caf3ddba951d685d
abuduru <abdulg1995@gmail.com>
2019-01-02 06:01:07 -0500

    added spinner class to display loading graphic

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,7 +4,7 @@ import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 class Modal extends Component{
     shouldComponentUpdate(nextProps,nextstate){
-        return nextProps.show !== this.props.show
+        return nextProps.show !== this.props.show || nextProps.children  !== this.props.children;
         
     }
     
